bb43fb0d9091e548fe1df999a880f194c900190f
0xKurt <Kurt@Merbeth.io>
2024-05-23 13:40:02 +0200

    add template

--- src/data/chains/11155111/chain.ts
+++ src/data/chains/11155111/chain.ts
@@ -55,7 +55,7 @@ export const sepolia: TChain = {
       code: "USDC",
       icon: "https://ipfs.io/ipfs/QmTBHBZchoxncW1LXCvptTbvRzuZDN8yzze3xXrRsh2WZz",
       address: "0x78e0D07C4A08adFfe610113310163b40E7e47e81",
-      decimals: 6,
+      decimals: 18,
       priceSource: {
         chainId: 1,
         address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
