8b82956e11d2049b0e484f66d225a267859a773a
0xKurt <Kurt@Merbeth.io>
2024-07-29 11:06:56 +0200

    Add Metis

---
+++ src/data/chains/1088/chain.ts
@@ -0,0 +1,58 @@
+import { TChain } from "../../../types";
+import { metisIcon } from "../../icons";
+
+// TODO: replace chainName with the actual chain name
+export const chainName: TChain = {
+  id: 1088, // chain id
+  name: "metis-andromeda", // chain name
+  prettyName: "Metis Andromeda", // pretty network name
+  type: "mainnet", // chain type: "mainnet" or "testnet"
+  blockExplorer: "https://andromeda-explorer.metis.io/",
+  icon: metisIcon, // imported svg icon
+  rpc: "https://andromeda.metis.io/?owner=1088", // public rpc url
+  contracts: {
+    multiRoundCheckout: "0x..",
+    quadraticFunding: "0x..",
+    directGrants: "0x..",
+  },
+  pricesFromTimestamp: 0, // timestamp to get prices from
+  tokens: [
+    {
+      code: "METIS", // native token symbol
+      icon: metisIcon, // imported svg icon
+      address: "0x0000000000000000000000000000000000000000",
+      decimals: 18,
+      canVote: true, // true for native currency and stable coins, protocol and other are false
+      priceSource: {
+        chainId: 1, // chain id
+        address: "0x9e32b13ce7f2e80a01932b42553652e053d6ed8e",
+      },
+      // see: https://github.com/redstone-finance/redstone-node/blob/main/src/config/tokens.json
+      redstoneTokenId: "METIS", // redstone token id
+    },
+    {
+      code: "METIS", // native token symbol
+      icon: metisIcon, // imported svg icon
+      address: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+      decimals: 18,
+      canVote: true,
+      priceSource: {
+        chainId: 1, // chain id
+        address: "0x9e32b13ce7f2e80a01932b42553652e053d6ed8e",
+      },
+      redstoneTokenId: "METIS", // redstone token id
+    },
+  ],
+  subscriptions: [
+    {
+      contractName: "AlloV2/Registry/V1",
+      address: "0x4aacca72145e1df2aec137e1f3c5e3d75db8b5f3", // contract address of Registry
+      fromBlock: 0, // block number
+    },
+    {
+      contractName: "AlloV2/Allo/V1",
+      address: "0x1133eA7Af70876e64665ecD07C0A0476d09465a1", // contract address of Allo
+      fromBlock: 0, // block number
+    },
+  ],
+};
