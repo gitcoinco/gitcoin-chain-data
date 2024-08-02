593de0eb06a28c512d9f5eb00e55a1be8b6546a2
0xKurt <Kurt@Merbeth.io>
2024-08-02 17:01:00 +0200

    publish

--- dist/data/chains/1/chain.ts
+++ dist/data/chains/1/chain.ts
@@ -27,6 +27,7 @@ export const mainnet: TChain = {
         chainId: 1,
         address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
       },
+      permitVersion: "2",
       redstoneTokenId: "USDC",
     },
     {
