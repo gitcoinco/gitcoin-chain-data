ce292fd90f7ec4521d4b15727805867176479030
eagle <aparokshambhargav@gmail.com>
2024-08-02 20:07:22 +0530

    feat: update permitVersion for base tokens

--- src/data/chains/8453/chain.ts
+++ src/data/chains/8453/chain.ts
@@ -26,6 +26,7 @@ export const base: TChain = {
         chainId: 1,
         address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
       },
+      permitVersion: "2",
       redstoneTokenId: "USDC",
     },
     {
