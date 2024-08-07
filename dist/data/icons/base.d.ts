9c0850772cafd505292a4d562bffbe6e2d0af214
0xKurt <Kurt@Merbeth.io>
2024-08-07 12:17:07 +0200

    rm dist

--- dist/data/icons/base.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: "<svg width=\"364\" height=\"364\" viewBox=\"0 0 364 364\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n<g clip-path=\"url(#clip0_1954_614)\">\n<path d=\"M181.68 364C282.377 364 364 282.522 364 182C364 81.4778 282.377 0 181.68 0C86.1515 0 7.7896 73.3533 0 166.697H240.983V197.303H0C7.7896 290.647 86.1515 364 181.68 364Z\" fill=\"black\"/>\n</g>\n<defs>\n<clipPath id=\"clip0_1954_614\">\n<rect width=\"364\" height=\"364\" fill=\"white\"/>\n</clipPath>\n</defs>\n</svg>\n";
-export default _default;
